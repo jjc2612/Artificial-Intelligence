7e4324bb5a8c683ebbe54d6a73ae6a2beb2246f0
jjc2612 <jjc2612@gmail.com>
2021-05-06 17:09:03 -0700

    Revert "first commit"
    This reverts commit 113b3ca6

---
+++ projects/2021/degrees/util.py
@@ -0,0 +1,41 @@
+class Node():
+    def __init__(self, person_id, parent, movie_id):
+        self.person_id = person_id
+        self.parent = parent
+        self.movie_id = movie_id
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, person_id):
+        return any(node.person_id == person_id for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+    def length(self):
+        return len(self.frontier)
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
